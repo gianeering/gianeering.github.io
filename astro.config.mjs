29bbfe18ac082957054c5b396493532f4c8baa00
Gianfranco Ponza <GianProgrammer@yahoo.it>
2026-01-26 23:38:14 +0100

    fixed astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,20 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import mdx from "@astrojs/mdx";
 
-const LOCALHOST_URL = 'http://localhost:${SERVER_PORT}';
-const LIVE_URL = 'https://gianeering.github.io';
+const SERVER_PORT = Number(process.env.SERVER_PORT || 4321);
+
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+const LIVE_URL = "https://gianeering.github.io";
+
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
-let BASE_URL = LOCALHOST_URL;
 
-if(isBuild){
-  BASE_URL = LIVE_URL
-}
+const BASE_URL = isBuild ? LIVE_URL : LOCALHOST_URL;
 
-// https://astro.build/config
 export default defineConfig({
-  server: {port: SERVER_PORT},
+  server: { port: SERVER_PORT },
   site: BASE_URL,
-  // Altre opzioni di configurazione
-  integrations: [
-    react(), mdx()
-  ],
-  vite: {
-    server: {
-      // Configurazioni specifiche del server, se necessarie
-    },
-    build: {
-      // Opzioni di build
-    },
-  },
+
+  integrations: [react(), mdx()],
 });
