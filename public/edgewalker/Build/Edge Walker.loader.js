02fb94060b446ce7ab0cc60a76ffcff33c5004ae
Gianfranco Ponza <GianProgrammer@yahoo.it>
2026-05-16 00:30:52 +0200

    added latest version of LDM project

--- public/edgewalker/Build/Edge Walker.loader.js
+++ public/edgewalker/Build/Edge Walker.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 65287390,
+    wasmFileSize: 65418730,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
