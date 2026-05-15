dc7c5378f8acede928ad45536f1630cb8607c565
Gianfranco Ponza <GianProgrammer@yahoo.it>
2026-05-15 21:53:34 +0200

    Fixing levels.xml not found

--- public/edgewalker/Build/Edge Walker.loader.js
+++ public/edgewalker/Build/Edge Walker.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 65178074,
+    wasmFileSize: 65270358,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
